20e7b540c45947058de3481b0bf6270e63ab64be
Vengad Alagarsamy <vengadalagarsamy@MacBook-Pro.local>
2020-10-29 19:43:50 +0000

    first commit

---
+++ Journal/EntryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Vengad Alagarsamy on 29/10/2020.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
